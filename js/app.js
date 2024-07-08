fcb79ae8969f44d117d2c8283ddf50ca0d9d0c51
Paul Gagne <bahkoo@gmail.com>
2024-07-07 19:02:09 -0500

    feat: add sction and more search terms

--- js/app.js
+++ js/app.js
@@ -1,44 +1,107 @@
-const searchTerms = [
-  "nintendo",
-  "nes",
-  "snes",
-  "gameboy",
-  "gamecube",
-  "n64",
-  "ds",
-  "3ds",
-  "wii",
-  "switch",
-  "",
-  "playstation",
-  "ps2",
-  "ps3",
-  "ps4",
-  "ps5",
-  "",
-  "xbox",
-  "xbone",
-  "",
-  "sega",
-  "dreamcast",
-  "game gear",
-  "",
-  "sale",
-  "garage sale",
-  "yard sale",
-  "estate sale"
+const sections = [
+  {
+    title: "Nintendo",
+    searchTerms: [
+      "switch",
+      "gamecube",
+      "n64",
+      "wii",
+      "ds",
+      "3ds",
+      "nes",
+      "snes",
+      "gameboy",
+      "nintendo",
+    ]
+  },
+  {
+    title: "Playstation",
+    searchTerms: [
+      "ps4",
+      "ps5",
+      "ps2",
+      "ps3",
+      "playstation",
+    ]
+  },
+  {
+    title: "Other Video Games",
+    searchTerms: [
+      "360",
+      "xbox",
+      "xbone",
+      "sega",
+      "dreamcast",
+      "game gear",
+    ]
+  },
+  {
+    title: "Toys",
+    searchTerms: [
+      "pokemon",
+      "amiibo"
+    ]
+  },
+  {
+    title: "VR",
+    searchTerms: [
+      "vr",
+      "vr2",
+      "psvr",
+      "oculus",
+      "meta"
+    ]
+  },
+  {
+    title: "Power Tools",
+    searchTerms: [
+      "dewalt",
+      "milwaukee",
+      "makita",
+      "rigid",
+      "bosch",
+      "ryobi"
+    ]
+  },
+  {
+    title: "Sales",
+    searchTerms: [
+      "garage sale",
+      "yard sale",
+      "clearance sale",
+      "liquidation sale",
+      "book sale",
+      "sale"
+    ]
+  }
 ];
 
 const daysSinceListed = 7;
 
-document.getElementById("openAll").addEventListener("click", openAll);
-
 const linksElement = document.getElementById("links");
 
-searchTerms.forEach(x => linksElement.appendChild(createLink(x)));
+sections.forEach(x => linksElement.appendChild(createSection(x)));
 
-function openAll() {
+function openAll(searchTerms) {
   searchTerms.forEach(searchTerm => window.open(`https://www.facebook.com/marketplace/category/search?daysSinceListed=${daysSinceListed}&sortBy=creation_time_descend&query=${searchTerm}&exact=true`));
+}
+
+function createSection(section)
+{
+  let div = document.createElement('div');
+  let title = document.createElement('h4');
+  let button = document.createElement('div');
+
+  title.innerText = section.title;
+  button.className = "button";
+  button.innerText = "Open All";
+  button.addEventListener("click", () => openAll(section.searchTerms));
+
+  div.appendChild(title);
+  section.searchTerms.forEach(x => div.appendChild(createLink(x)));
+  div.appendChild(button);
+
+  return div;
 }
 
 function createLink(searchTerm) {
